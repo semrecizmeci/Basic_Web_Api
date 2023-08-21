1a543f44d56a37ef02b1431e69aaf783559bba56
Sadık Emre Çizmecioğlu <121570540+semrecizmeci@users.noreply.github.com>
2023-08-21 15:28:30 +0300

    Add files via upload

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mvcApideneme.Models;
+
+namespace mvcApideneme.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost("postdata")]
+        public IActionResult PostData([FromBody] DataModel model)
+        {
+            // Alınan model verisi ile yapılacak işlemleri gerçekleştirin
+            // Örneğin: Veritabanına kaydetme işlemi
+            if (model.name==null)
+            {
+                return Ok("api den geliyorum, null geldi");
+            }
+            return Ok("galiba tamamım");
+
+        }
+
+        //private IActionResult Ok(string name, string v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ WebApplication1/Models/DataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace mvcApideneme.Models
+{
+    public class DataModel
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+    }
+}
